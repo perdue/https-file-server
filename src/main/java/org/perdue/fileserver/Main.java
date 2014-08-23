4c669760b19952f484ed90b2cac29a4272906fef
Brent Perdue <perdue@ccri.com>
2014-08-22 23:03:59 -0400

    Now can pass port number in on command line

--- src/main/java/org/perdue/fileserver/Main.java
+++ src/main/java/org/perdue/fileserver/Main.java
@@ -8,36 +8,44 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.*;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
+
 public class Main {
     private static final Logger LOG = Log.getLogger(Main.class.toString());
 
-        
-      public static void main(String[] args) throws Exception
-      {
+    public static final int DEFAULT_PORT = 8080;
+
+    public static void main(String[] args) throws Exception
+    {
+        final int port = getPort(args);
   
-          //The port that we should run on can be set into an environment variable
-          //Look for that variable and default to 8080 if it isn't there.
-          String port = System.getenv("PORT");
-          if(port == null || port.isEmpty()) {
-              port = "8080";
-          }
-          
-    	  Server server = new Server(Integer.valueOf(port));
+        Server server = new Server(port);
   
-          ResourceHandler resource_handler = new ResourceHandler();
-          resource_handler.setDirectoriesListed(true);
-          resource_handler.setWelcomeFiles(new String[]{ "index.html" });
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setWelcomeFiles(new String[]{ "index.html" });
   
-          resource_handler.setResourceBase(".");
-          LOG.info("serving " + resource_handler.getBaseResource());
-          
-          HandlerList handlers = new HandlerList();
-          handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
-          server.setHandler(handlers);
-  
-          server.start();
-          server.join();
-      }
-      
+        resource_handler.setResourceBase(".");
+        LOG.info("serving " + resource_handler.getBaseResource());
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
+        server.setHandler(handlers);
+
+        server.start();
+        server.join();
+    }
+
+    private static int getPort(final String[] args) {
+        if(args.length == 1) {
+            try {
+                return Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Error parsing port number as an integer: "+ args[0]);
+                System.exit(-1);
+            }
+        }
+        return DEFAULT_PORT;
+    }
+
 
 }
